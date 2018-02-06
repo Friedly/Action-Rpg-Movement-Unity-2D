0234965e35181cfd3445587b6fc60f50e7bda6ff
Julien Saevecke <Julien.Saevecke@udo.edu>
2018-02-06 01:25:14 +0100

    8 Directional Movement and Camera
    Add 8 Directional Movement Character with Unitys Physic System
    Add Camera that smoothly follows the player
    Change some collision detection settings to prevent the player from getting stuck
    Change some graphic settings to prevent pixel artifacts

---
+++ Action-Rpg-Movement-Unity-2D/Assets/Scripts/FollowGameObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGameObject : MonoBehaviour {
+
+    public GameObject goToFollow;
+    public float followSpeed = 5f;
+
+    private Vector3 position;
+
+    private void Start()
+    {
+        if(goToFollow != null)
+        {
+            position = goToFollow.transform.position;
+            position.z = -10;
+        }
+        else
+        {
+            Debug.LogError("Can not follow a null reference!");
+        }
+     }
+
+    void Update ()
+    {
+        position = goToFollow.transform.position;
+        position.z = -10;
+
+        this.transform.position = Vector3.Lerp(this.transform.position, position, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Action-Rpg-Movement-Unity-2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+    private Vector2 input = new Vector2();
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate ()
+    {
+        rigidBody.MovePosition(rigidBody.position + input.normalized * moveSpeed);
+        //rigidBody.AddForce(input * moveSpeed, ForceMode2D.Force);
+    }
+}
